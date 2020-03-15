f0f8c1c12cfd70ef531d9c7b8645b5a5c7717f39
lvchkn <you@example.com>
2020-03-16 00:46:37 +0300

    Revert "Revert "added unit tests, passed them well""
    This reverts commit 3ed76e849cb1685607ffc20094709171441f8dc3.

--- 1laba6variant/Solution.cs
+++ 1laba6variant/Solution.cs
@@ -2,34 +2,44 @@
 
 namespace _1laba6variant
 {
-    class Solution : ProgInterface
+    public class Solution : IProgInterface
     {
-        public double F(double x)
+       /* public double F(double x)
         {
             return (322 * x) - Math.Log(11 * x) - 2;
-        }
+        }*/
 
 
-        public double Simpson(double a, double b, double n)
+        public double Simpson(double a, double b, double n, Func<double, double> func)
         {
+            if (n < 0)
+            {
+                throw new ArgumentException();
+            }
+
             double s = 0, h = (b - a) / n;
 
-            double I2 = 0, I4 = F(a + h);
+            double I2 = 0, I4 = func(a + h);
 
             for (int k = 2; k < n; k += 2)
             {
-                I2 += F(a + k * h);
-                I4 += F(a + (k + 1) * h);
+                I2 += func(a + k * h);
+                I4 += func(a + (k + 1) * h);
             }
-            s = F(a) + F(b) + 4 * I4 + 2 * I2;
+            s = func(a) + func(b) + 4 * I4 + 2 * I2;
             s *= h / 3;
 
             return s;
 
         }
 
-        public double Rectangle(double a, double b, double n)
+        public double Rectangle(double a, double b, double n, Func<double, double> func)
         {
+            if (n < 0)
+            {
+                throw new ArgumentException();
+            }
+
             float k = 0.5f;
             double h = (b - a) / n;
             double sumrect = 0;
@@ -38,7 +48,7 @@ namespace _1laba6variant
 
             for (int i = 0; i < n; i++)
             {
-                sumrect += F(a + (h * i));
+                sumrect += func(a + (h * i));
             }
 
             return h * sumrect;
---
+++ UnitTestIntegrals/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestIntegrals")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestIntegrals")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2c1e08a-39d4-4ae4-96cc-33a9c0cc103c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
